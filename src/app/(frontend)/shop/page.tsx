366534d75608a01facdbd80e62dfaf6fd98d9d04
Mohamed-Waled <mohamedwaledfarag@gmail.com>
2025-08-25 08:45:05 +0300

    CPG-42 Separated the page header into a separate component to be more reusable

--- src/app/(frontend)/shop/page.tsx
+++ src/app/(frontend)/shop/page.tsx
@@ -1,22 +1,14 @@
-import { cabin, eduNSWACTCursive } from "@/utils/fonts";
+import PageHeading from "@/components/page-heading/PageHeading";
 import classes from "./page.module.scss";
-import Breadcrumb from "@/helper_components/breadcrumb/Breadcrumb";
 
 function page() {
   return (
     <>
       <section className={classes["home-page"]}>
-        <Breadcrumb />
-        <h1
-          className={`${classes["home-page__heading-text"]} ${cabin.className}`}
-        >
-          Shopping
-        </h1>
-        <p
-          className={`${classes["home-page__description-text"]} ${eduNSWACTCursive.className}`}
-        >
-          Life’s short. Buy the clothes, wear the smile.
-        </p>
+        <PageHeading
+          headingText="Shopping"
+          descriptionText="Life’s short. Buy the clothes, wear the smile."
+        />
       </section>
     </>
   );
